c59519e232e12e0cb139ccbb7d6e675999ef62cc
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-03-26 18:31:14 +0100

    added empty theme container

--- script.js
+++ script.js
@@ -9,9 +9,14 @@ var max_number_of_themes = 5;
 var number_of_themes = 1;
 
 // 
+// Theme - Container
+//
+var theme_container = document.getElementById("empty_theme_container");
+
+
+// 
 // Main
 // 
-
 
 // Function: Add Theme
 function addTheme() {
@@ -19,23 +24,14 @@ function addTheme() {
     // Check if the number of themes is less than the maximum number of themes
     if (number_of_themes <= max_number_of_themes) {
 
-        // Add a new theme div 
-        var new_theme = document.createElement("div");
-        // Add a class
+        // Create a new theme div 
+        var new_theme = theme_container.cloneNode(true);
+        // Add the right class
         new_theme.className = "theme_container";
-        // Add an id containing the number
+        // Add an id containing the container number
         new_theme.id = "theme_container_" + number_of_themes;
         // Add the theme div to the theme container
         document.getElementById("generator_container").appendChild(new_theme);
-
-        // Create a theme delete button
-        var theme_delete_button = document.createElement("div");
-        // Add a class
-        theme_delete_button.className = "theme_delete_button";
-        // Add an X to the theme delete button
-        theme_delete_button.innerHTML = "<div onclick='removeTheme(" + number_of_themes + ")' class='theme_delete_button_x'>X</div>";
-        // Add the theme delete button to the theme container
-        document.getElementById("theme_container_" + number_of_themes).appendChild(theme_delete_button);
 
         // Increase the number of themes
         number_of_themes++;
@@ -43,9 +39,9 @@ function addTheme() {
 }
 
 // Function: Remove Theme
-function removeTheme(theme_number) {
+function removeTheme(element) {
     // Remove the theme container
-    document.getElementById("theme_container_" + theme_number).remove();
+    document.getElementById(element.parentNode.parentNode.id).remove();
     // Increase the max number of themes
     max_number_of_themes++;
 }
